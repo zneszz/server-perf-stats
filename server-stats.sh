01a18e5c80f70684060b8a027829b78887766efa
Arthur <znesz@proton.me>
2026-03-17 18:58:47 -0300

    fix: include percentage

--- server-stats.sh
+++ server-stats.sh
@@ -12,4 +12,4 @@ free | awk '/Mem/ {printf("used: %sMB / total: %sMB (%.2f%%)\n", $3,$2,$3/$2*100
 
 echo ""
 echo "disk:"
-df -h / | grep /dev/ | awk '{printf("used: %s / avail: %s / size: %s\n", $3, $4, $2)}'
+df -h / | grep /dev/ | awk '{printf("used: %s / avail: %s / perc: %s\n", $3, $4, $5)}'
