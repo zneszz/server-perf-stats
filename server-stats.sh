a89d92dc5a480dd0907a9712f254ef2bf0da8711
Arthur <znesz@proton.me>
2026-03-17 19:13:15 -0300

    feat: top ram and cpu usage

--- server-stats.sh
+++ server-stats.sh
@@ -13,3 +13,12 @@ free | awk '/Mem/ {printf("used: %sMB / total: %sMB (%.2f%%)\n", $3,$2,$3/$2*100
 echo ""
 echo "disk:"
 df -h / | grep /dev/ | awk '{printf("used: %s / avail: %s / perc: %s\n", $3, $4, $5)}'
+
+echo ""
+echo "top 5 by ram usage:"
+ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6
+
+echo ""
+echo "top 5 by cpu usage:"
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6
+
