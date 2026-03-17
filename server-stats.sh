3e08054734288c09a27f3016418cb81acd7c8335
Arthur <znesz@proton.me>
2026-03-17 18:56:54 -0300

    feat: disk usage

--- server-stats.sh
+++ server-stats.sh
@@ -9,3 +9,7 @@ top -bn1 | grep "Cpu(s)" | awk '{print "usage: " $2 + $4 "%"}'
 echo ""
 echo "ram:"
 free | awk '/Mem/ {printf("used: %sMB / total: %sMB (%.2f%%)\n", $3,$2,$3/$2*100)}'
+
+echo ""
+echo "disk:"
+df -h / | grep /dev/ | awk '{printf("used: %s / avail: %s / size: %s\n", $3, $4, $2)}'
